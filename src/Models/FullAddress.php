239120dc3b3b586ae38f8c5e2977e5175a855aea
Aryel Mota Góis <aryel.gois@gmail.com>
2017-10-17 18:04:55 -0300

    Add FullAddress Model

---
+++ src/Models/FullAddress.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * This Software is part of aryelgois\MedooWrapper and is provided "as is".
+ *
+ * @see LICENSE
+ */
+
+namespace aryelgois\MedooWrapper\Models;
+
+use aryelgois\MedooWrapper;
+
+/**
+ * A Full Address object to reference a specific place in the world
+ *
+ * This class allows you to use more specific information about the address,
+ * which should be provided by your clients.
+ *
+ * NOTES:
+ * - It requires a contrete Address class in the same namespace as the child
+ *
+ * @author Aryel Mota Góis
+ * @license MIT
+ * @link https://www.github.com/aryelgois/medoo-wrapper
+ */
+abstract class FullAddress extends MedooWrapper\DatabaseObject
+{
+    const DATABASE_TABLE = 'full_addresses';
+
+    /**
+     * Rows fetched from `full_address`
+     *
+     * @const string[]
+     */
+    const ROWS_FULL_ADDRESS = [
+        'id',
+        'county',
+        'neighborhood',
+        'place',
+        'number',
+        'zipcode',
+        'detail',
+        'update'
+    ];
+
+    /**
+     * Creates a new FullAddress object
+     *
+     * @param mixed[] $where Medoo where clause
+     */
+    public function __construct($where)
+    {
+        parent::__construct();
+
+        $full_address = $this->database->get(
+            static::DATABASE_TABLE,
+            static::ROWS_FULL_ADDRESS,
+            $where
+        );
+
+        $this->valid = false;
+        if ($full_address) {
+            $address_class = array_slice(explode('\\', static::class), 0, -1);
+            $address_class = implode('\\', $address_class) . '\Address';
+
+            $address = new $address_class(['id' => $full_address['county']]);
+            unset($full_address['county']);
+
+            if ($address->isValid()) {
+                $this->data = $full_address;
+                $this->data['address'] = $address;
+                $this->valid = true;
+            }
+        }
+    }
+}
